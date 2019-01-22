2a1b89db4ccafb785db4758c6e6dcd200b340a1c
Niall Eccles <nialle@rothco.ie>
2019-01-22 08:53:03 +0000

    Changed floating action button to a diamond. Added in settings page.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class _DiamondBorder extends ShapeBorder {
+  const _DiamondBorder();
+
+  @override
+  EdgeInsetsGeometry get dimensions {
+    return const EdgeInsets.only();
+  }
+
+  @override
+  Path getInnerPath(Rect rect, { TextDirection textDirection }) {
+    return getOuterPath(rect, textDirection: textDirection);
+  }
+
+  @override
+  Path getOuterPath(Rect rect, { TextDirection textDirection }) {
+    return Path()
+      ..moveTo(rect.left + rect.width / 2.0, rect.top)
+      ..lineTo(rect.right, rect.top + rect.height / 2.0)
+      ..lineTo(rect.left + rect.width  / 2.0, rect.bottom)
+      ..lineTo(rect.left, rect.top + rect.height / 2.0)
+      ..close();
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, { TextDirection textDirection }) {}
+
+  // This border doesn't support scaling.
+  @override
+  ShapeBorder scale(double t) {
+    return null;
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -117,7 +150,8 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        shape: _DiamondBorder(),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  
+  SettingsPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.blueAccent,
+      child:  new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text('Your Score: ', style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 50.0),),
+          new IconButton(
+            icon: new Icon(Icons.arrow_forward),
+            color: Colors.white,
+            iconSize: 50.0,
+            //  onPressed: () => Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) =>new LandingPage()), (Route route) => route == null),
+          )
+        ],
+      ),
+    );
+  }
+}
